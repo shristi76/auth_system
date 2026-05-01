8f4a0a670ca69db24f73085dfee19fc6987593bc
shristi76 <shristishri426@gmail.com>
2026-05-01 16:02:28 +0530

    connect

--- auth_proj/backend/src/app.js
+++ auth_proj/backend/src/app.js
@@ -7,7 +7,8 @@ const app=express();
 app.use(express.json());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",
+    "https://authsystem5.vercel.app"],
     credentials:true
 }))
 
